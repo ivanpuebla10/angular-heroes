28f89ffa4987481e8b6671591a5aaa4d64ae9a67
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-08-22 19:47:10 +0200

    fake server created and setted

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,11 +3,13 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
+  
 getHeroes(): Observable<Hero[]> {
   const heroes = of(HEROES);
   this.messageService.add('HeroService: fetched heroes');
@@ -19,5 +21,11 @@ getHero(id: number): Observable<Hero> {
   this.messageService.add(`HeroService: fetched hero id=${id}`);
   return of(hero);
 }
-  constructor(private messageService: MessageService) { }  
+constructor(
+  private http: HttpClient,
+  private messageService: MessageService) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 }
