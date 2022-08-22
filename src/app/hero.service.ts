2f666161d31d82c2577064bf4991c7478c6cc705
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-08-22 19:50:58 +0200

    handle error function

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,8 +13,23 @@ export class HeroService {
 
 
   getHeroes(): Observable<Hero[]> {
-  return this.http.get<Hero[]>(this.heroesUrl)
-}
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      console.error(error); 
+  
+      this.log(`${operation} failed: ${error.message}`);
+  
+      return of(result as T);
+    };
+  }
+  
 
 getHero(id: number): Observable<Hero> {
 
